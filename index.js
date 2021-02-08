50d6e6e188aee969be53827ac5b9e79e38d46fa9
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-02-08 18:24:24 -0300

    add body-parser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //importações
 const express = require("express");
+const bodyParser = require("body-parser");
 //instanciando
 const app = express();
 //set { usando o EJS como engine}
 app.set("view engine", "ejs");
 app.use(express.static("public")); //definindo a pasta de arquivos estatico
+//configurando o bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //rotas
 
@@ -14,6 +18,15 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+
+  res.send(
+    "Formulario recebido! Titulo: " + titulo + " " + "Descrição: " + descricao
+  );
 });
 
 //criando o server da aplicação
