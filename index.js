b52e144a8b4ef084688bb3d33d65c26559fb496d
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-02-08 14:18:50 -0300

    condicional no ejs

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const express = require("express");
 //instanciando
 const app = express();
 //set { usando o EJS como engine}
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //rotas
 
 app.get("/:nome/:lang", (req, res) => {
-  var nome = req.params.nome
-  var lang = req.params.lang
+  var nome = req.params.nome;
+  var lang = req.params.lang;
+  var exibirMsg = true;
   res.render("index", {
     nome: nome,
     lang: lang,
     empresa: "Guia do programador",
-    inscritos: 8000
+    inscritos: 10000,
+    msg: exibirMsg,
   });
 });
 
